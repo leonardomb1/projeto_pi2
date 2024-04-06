39222cc5fdca404ccacc0d22f325155910d2cd07
leonardomb1 <lmbaudin2013@gmail.com>
2024-04-06 11:39:02 -0300

    removendo pkg e bodyparser

--- src/server.js
+++ src/server.js
@@ -1,12 +1,7 @@
-// const express = ("express");
 import express from "express"
-import pkg from 'body-parser';
-const { bodyParser } = pkg;
-// const res = require("express/lib/response");
 import sqlite3 from "sqlite3";
 const sqlite = sqlite3.verbose();
 const app = express();
-// const bodyParser = require("body-parser");
 let sql;
 const db = new sqlite.Database("./main.db", sqlite.OPEN_READWRITE, (err) => {
   if (err) return console.error(err);
