ead97ba55cc011720a0d5add030d127e2ec9e514
leonardomb1 <lmbaudin2013@gmail.com>
2024-06-09 21:59:43 -0300

    Fix: Melhoria rota de cartoes-pilares

--- src/controller/cartoes_pilares.controller.js
+++ src/controller/cartoes_pilares.controller.js
@@ -2,29 +2,41 @@ import CartoesPilares from '../models/cartoes_pilares.model.js'
 import returnClass from '../types/returnClass.js'
 
 export default class CartoesPilaresController {
-    static async create(req, res) {
-        const erros = validationResult(req)
-        if(!erros.isEmpty()){
-          return res.status(400).json({erros: erros.array()})
-        }
-    
-        const { id_cartao, id_pilar } = req.body;
-        let retorno = {};
-    
-        try {
-          const createdCartoesPilares = await CartoesPilares.create({
-            data: {
-                id_pilar,
-                id_cartao
+  static async create(req, res) {
+      const erros = validationResult(req)
+      if(!erros.isEmpty()){
+        return res.status(400).json({erros: erros.array()})
+      }
+  
+      const { id_cartao, nome_pilar } = req.body;
+      let retorno = {};
+  
+      try {
+        const createdCartoesPilares = [];
+        for (const pilar of nome_pilar) {
+          const pilarPeloNome = await Pilar.findUnique({
+            where: {
+              nome: pilar
             }
           });
-    
-          retorno = new returnClass("Sucesso!", 201, true, false, createdCartoesPilares);
-          return res.status(201).json(retorno);
-        } catch (error) {
-          console.log(error);
-          retorno = new returnClass("Erro Interno Servidor", 500, false, true, undefined);
-          return res.status(500).json(retorno);
+          
+          const cartaoPilar = await CartoesPilares.create({
+            data: {
+                id_cartao,
+                id_pilar: pilarPeloNome.id
+            }
+          });
+          createdCartoesPilares.push(cartaoPilar);
         }
-    }
+  
+        retorno = new returnClass("Sucesso!", 201, true, false, createdCartoesPilares);
+        return res.status(201).json(retorno);
+      } catch (error) {
+        console.log(error);
+        retorno = new returnClass("Erro Interno Servidor", 500, false, true, undefined);
+        return res.status(500).json(retorno);
+      }
+  }
 }
+
+
