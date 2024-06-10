3831c15437f5241b7816662212d6331568d95501
leonardomb1 <lmbaudin2013@gmail.com>
2024-06-09 22:06:27 -0300

    Fix: Melhoria rota de cartoes-pilares

--- src/validations/cartoes_pilares.validation.js
+++ src/validations/cartoes_pilares.validation.js
@@ -2,6 +2,5 @@ import { body } from "express-validator"
 
 
 export const createCartaoPilarValidation = [
-    body('id_pilar').isNumeric().withMessage('ID do Cartão é obrigatório!'),
-    body('id_cartao').isNumeric().withMessage('ID do Pilar é obrigatório!'),
+    body('id_cartao').isNumeric().withMessage('ID do Cartão é obrigatório!')
 ]
